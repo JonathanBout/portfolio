10f15b6cf95b17f61c08e8e5288ccc58fb9ad702
Jonathan Bout <jonathanbout01@gmail.com>
2023-09-02 20:30:22 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="title">Hello there!</h1>
+    <ul class="links">
+      <li><a href="https://github.com/jonathanbout">My GitHub</a></li>
+    </ul>
+  </main>
+</template>
+
+<style scoped lang="less">
+main {
+  text-align: center;
+  position: absolute;
+  inset: 0;
+}
+.links {
+  list-style-type: none;
+  margin: auto;
+  padding: 0;
+}
+</style>
