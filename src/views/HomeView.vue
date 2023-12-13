9367d2003084562960886a5b7b5b348714b0e609
Jonathan Bout <jonathanbout01@gmail.com>
2023-12-13 17:49:15 +0100

    no animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,23 +13,5 @@
 <style scoped lang="less">
 .fun-thing {
   font-size: 3rem;
-  outline: 1px solid transparent;
-
-  &:hover {
-    /* animate to begin scaling up and down */
-    animation: fun-thing 1s ease-in-out infinite;
-  }
-}
-
-@keyframes fun-thing {
-  0% {
-    font-size: 3rem;
-  }
-  50% {
-    font-size: 20rem;
-  }
-  100% {
-    font-size: 3rem;
-  }
 }
 </style>
