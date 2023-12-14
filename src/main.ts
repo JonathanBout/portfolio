fca3099f9cfab707aef292aec0528c6e713279f0
Jonathan Bout <jonathanbout01@gmail.com>
2023-12-14 11:57:35 +0100

    prettier

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import './assets/main.less'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "./assets/main.less"
+import "bootstrap-icons/font/bootstrap-icons.css"
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "../views/HomeView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView
     },
     {
       // fallback to 404 page
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: () => import('../views/NotFoundView.vue')
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: () => import("../views/NotFoundView.vue")
     }
   ]
 })
