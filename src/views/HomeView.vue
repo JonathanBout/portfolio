ac47a7fceae5f39fc3b4c86699f1a1380495faf6
Jonathan Bout <jonathanbout01@gmail.com>
2023-12-14 11:36:40 +0100

    fun

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <template>
   <main>
     <span>
-      <a href="https://github.com/jonathanbout" class="no-color fun-thing">
-        <i class="bi bi-github"></i>
-      </a>
+      <a href="https://github.com/jonathanbout" class="bi bi-github no-color fun-thing"> </a>
     </span>
   </main>
 </template>
@@ -11,5 +9,49 @@
 <style scoped lang="less">
 .fun-thing {
   font-size: 3rem;
+  line-height: 3rem;
+  display: block;
+  animation: rainbow-colors 60s linear infinite;
+  transition: color 10s ease-in-out;
+
+  &:not(:hover) {
+    color: var(--color-text) !important;
+  }
+}
+
+@keyframes rainbow-colors {
+  0% {
+    color: #ff0000;
+  }
+  10% {
+    color: #ff8000;
+  }
+  20% {
+    color: #ffff00;
+  }
+  30% {
+    color: #80ff00;
+  }
+  40% {
+    color: #00ff00;
+  }
+  50% {
+    color: #00ff80;
+  }
+  60% {
+    color: #00ffff;
+  }
+  70% {
+    color: #0080ff;
+  }
+  80% {
+    color: #0000ff;
+  }
+  90% {
+    color: #8000ff;
+  }
+  100% {
+    color: #ff0000;
+  }
 }
 </style>
