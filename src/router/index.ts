0b56b1acf89eb501fa617c21ef613f6b1ad6e850
Jonathan Bout <jonathanbout01@gmail.com>
2024-02-02 22:12:05 +0100

    privacy policy & robots.txt

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import PrivacyPolicyJSONWatchfaceView from "../views/PrivacyPolicyJSONWatchfaceView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView
+    },
+    {
+      path: "/json-watchface-privacy-policy",
+      name: "JSON Watch Face Privacy Policy",
+      component: PrivacyPolicyJSONWatchfaceView
     },
     {
       // fallback to 404 page
